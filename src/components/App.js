204435576fdb9b35fe77517dd33fa259b0a4e5d8
Stercy Mutuku <mutukustercy93@gmail.com>
2025-07-29 13:06:52 +0300

    Uses the useEffect hook to fetch data from an API and set state

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
-// create your App component here
+//App component
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState();
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => setDogImage(data.message));
+  }, []);
+
+  if (!dogImage) {
+    return <p>Loading...</p>;
+  }
+
+  return <img src={dogImage} alt="A Random Dog" />;
+}
+
+export default App;
